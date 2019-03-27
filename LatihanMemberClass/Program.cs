a7e4f596138c718119fa1b9bc68975b9109f2afa
Your Name <you@example.com>
2019-03-28 05:31:41 +0700

    first commit

---
+++ LatihanMemberClass/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxi taxi = new Taxi();
+
+            taxi.DriverName = "Ari";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 4;
+
+            taxi.TaxiInfo();
+            {
+                if (taxi.OnDuty == true)
+                {
+                    taxi.PickUpPassenger();
+                }
+                else
+                {
+                    taxi.DropOffPassenger();
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LatihanMemberClass/Taxi.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMemberClass
+{
+    class Taxi
+    {
+        //properties
+        public string DriverName { set; get; }
+        public bool OnDuty { set; get; }
+        public int NumPassenger { set; get; }
+
+        //method
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name : {0}", DriverName);
+            Console.WriteLine("On Duty : {0}", OnDuty ? "Yes" : "No");
+            Console.WriteLine("Number Of Passenger : {0}", NumPassenger);
+
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("Driver {0} is busy", DriverName);
+        }
+
+        public void DropOffPassenger()
+        {
+            Console.WriteLine("Driver {0} available", DriverName);
+        }
+    }
+}
